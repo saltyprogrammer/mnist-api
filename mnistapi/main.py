d49dd1099f9449b554eaf6c19e83c71b253f6e2f
Jesus Fonseca <jefonbu@gmail.com>
2022-03-16 17:19:42 -0600

    Public API

--- mnistapi/main.py
+++ mnistapi/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from mnistapi.router import image_post
+
+origins = ["*"]
 
 app = FastAPI()
 app.include_router(image_post.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
